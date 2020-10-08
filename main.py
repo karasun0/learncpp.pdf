3cbb855372a579c05602bb32fbf41983b70d9210
karasun0 <amirrj9rjr@gmail.com>
2020-10-08 12:56:38 +0330

    You can see in it. I haven't done anything yet. Just Added two main variables and imported dependencies.

---
+++ main.py
@@ -0,0 +1,5 @@
+from pdfkit import from_url # from_url()
+from bs4 import BeautifulSoup # soup, 
+
+site_index_urls = [] # For learncpp.com contents urls
+size_index_print_urls = [] # For content's print urls
