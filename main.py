4548e5f327815c414d8065acc7d1a26a07f356e6
karasun0 <amirrj9rjr@gmail.com>
2020-10-08 13:59:27 +0330

    Added urls_to_pdf function. And finished main(start) part

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pdfkit import from_url  # from_url()
 from bs4 import BeautifulSoup  # BeautifulSoup
 from requests import get  # get()
 
+
 def get_site_index_urls() -> list:
     """
         Description: For fetching https://www.learncpp.com site index urls and return it.
@@ -30,12 +31,29 @@ def site_index_print_urls(raw_urls: list) -> list:
         Parameters: raw_urls: list
         Return type: list
     """
-    print_urls = [] # The list that raw_urls will append to it. This will be returned
+    print_urls = []  # The list that raw_urls will append to it. This will be returned
     for url in raw_urls:
-        print_urls.append(url + "print/") # Adding "print/" for having printable url
-    return print_urls # Returning them
+        # Adding "print/" for having printable url
+        print_urls.append(url + "print/")
+    return print_urls  # Returning them
+
+
+def urls_to_pdf(urls: list) -> None:
+    """
+        Description: Turning urls to PDF format
+        Parameters: urls: list
+        Return type: None
+    """
+    from_url(urls, "learncpp.pdf") # Turning all url to PDF format
 
 
 if __name__ == "__main__":
+    print("Hello. I'll be doing my job. It is going to be long(Depends on Internet speed).\nMake sure you have installed requirement.")
+
     site_index_urls = get_site_index_urls()  # site index urls
-    site_index_print_urls = site_index_print_urls(site_index_urls)  # For content's print urls
+    site_index_print_urls = site_index_print_urls(
+        site_index_urls)  # For content's print urls
+
+    urls_to_pdf(site_index_print_urls) # Turning all url to PDF format
+
+    print("Finished.")
